ad60daf161fd3c3c840df16235d774e80177562c
leeweiminsg <lee.wei.min@u.nus.edu>
2020-08-31 23:43:02 +0800

    Add Studio 4 Materials

---
+++ studios/studio_4/studio_4.js
@@ -0,0 +1,28 @@
+// cedric
+
+function pascal(row, position) {
+  return row === position || position === 1
+    ? 1
+    : pascal(row - 1, position - 1) + pascal(row - 1, position);
+}
+
+pascal(5, 3);
+
+// raja part 2
+
+//Qn1 Mukund and Vinh
+math_sqrt(math_log(math_E)); // answer is 1
+math_log(math_sqrt(math_E * math_E)); // answer is 1
+
+//Qn 2 trushti and jodi
+math_sqrt(math_sqrt(math_sqrt(6561)))(
+  //Qn3 Shee Hui and Shivam(part 1)
+  //    Cedric and Quoc Huy (Part 2)
+
+  //Qn 4 (a) Shee Hui and Shivam
+  (thrice, thrice)(add1)(6) // answer is 33
+// (b) Quoc Huy and Cedric
+// Answer: compose
+// (c) Quang Vinh and Raja
+// Answer: 1
+// (d) Trushti and Jodi
